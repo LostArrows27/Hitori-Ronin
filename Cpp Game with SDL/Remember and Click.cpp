5013c793e9bdb9a9f4e65db81ac1646e2b6d4c2b
Nguyễn Thành Dũng <thelastofus2isnuts@gmail.com>
2022-03-16 15:22:16 +0700

    Stretching Image to fit screen window
    stretching an image fit in window using SDL_BlitScaled and SDL_Rect

--- Cpp Game with SDL/Remember and Click.cpp
+++ Cpp Game with SDL/Remember and Click.cpp
@@ -1,82 +1,56 @@
 #include<iostream>
 #include<SDL.h>
 
-//using namespace std;
+const int SCREEN_HEIGHT = 800;
+const int SCREEN_WIDTH = 800;
 
-enum KeyPressSurfaces
-{
-    KEY_PRESS_SURFACE_DEFAULT, //0
-    KEY_PRESS_SURFACE_UP,      //1
-    KEY_PRESS_SURFACE_DOWN,    //2
-    KEY_PRESS_SURFACE_LEFT,    //3
-    KEY_PRESS_SURFACE_RIGHT,   //4
-    KEY_PRESS_SURFACE_TOTAL    //5
-};
-
-const int SCREEN_HEIGHT = 480;
-const int SCREEN_WIDTH = 640;
 SDL_Window* window = NULL;
 SDL_Surface* surface = NULL;
-SDL_Surface* keypresssurface[KEY_PRESS_SURFACE_TOTAL];
-// tao surface cho tung phim
-// de in tung anh tuong ung voi tung phim
-SDL_Surface* current = NULL;
+
+SDL_Surface* stretchedImage = NULL;
+
+void init()
+{
+    window = SDL_CreateWindow("Convert Image", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    surface = SDL_GetWindowSurface(window);
+}
 
 SDL_Surface* loadSurface(std::string path)
 {
+    SDL_Surface* optimized = NULL;
     SDL_Surface* temp = SDL_LoadBMP(path.c_str());
-    return temp;
+    optimized = SDL_ConvertSurface(temp, surface->format, 0);
+    SDL_FreeSurface(temp);
+    return optimized;
 }
 
 void close()
 {
-    SDL_FreeSurface(current);
-    current = NULL;
+    SDL_FreeSurface(stretchedImage);
+    stretchedImage = NULL;
     SDL_DestroyWindow(window);
     SDL_Quit();
 }
 
 int main(int argc, char* args[])
 {
-    window = SDL_CreateWindow("Testing", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    surface = SDL_GetWindowSurface(window);
+    init();
+    stretchedImage = loadSurface("5th_lesson_image/stretch.bmp");
     bool quit = false;
     SDL_Event e;
-    keypresssurface[KEY_PRESS_SURFACE_DEFAULT] = loadSurface("3rd_lesson_image/press.bmp");
-    keypresssurface[KEY_PRESS_SURFACE_UP] = loadSurface("3rd_lesson_image/up.bmp");
-    keypresssurface[KEY_PRESS_SURFACE_DOWN] = loadSurface("3rd_lesson_image/down.bmp");
-    keypresssurface[KEY_PRESS_SURFACE_LEFT] = loadSurface("3rd_lesson_image/left.bmp");
-    keypresssurface[KEY_PRESS_SURFACE_RIGHT] = loadSurface("3rd_lesson_image/right.bmp");
-    current = keypresssurface[KEY_PRESS_SURFACE_DEFAULT];
     while(!quit)
     {
         while(SDL_PollEvent(&e) != 0)
         {
-            if(e.type == SDL_QUIT) quit =true;
-            else if(e.type = SDL_KEYDOWN)
-            {
-                switch(e.key.keysym.sym)
-                {
-                    case SDLK_UP:
-                    current = keypresssurface[KEY_PRESS_SURFACE_UP];
-                    break;
-                    case SDLK_DOWN:
-                    current = keypresssurface[KEY_PRESS_SURFACE_DOWN];
-                    break;
-                    case SDLK_RIGHT:
-                    current = keypresssurface[KEY_PRESS_SURFACE_RIGHT];
-                    break;
-                    case SDLK_LEFT:
-                    current = keypresssurface[KEY_PRESS_SURFACE_LEFT];
-                    break;
-                    default:
-                    current = keypresssurface[KEY_PRESS_SURFACE_DEFAULT];
-                    break;
-                }
-            }
-            SDL_BlitSurface(current, NULL, surface, NULL);
-            SDL_UpdateWindowSurface(window);
+            if(e.type == SDL_QUIT) quit = true;
         }
+        SDL_Rect stretchRect;
+        stretchRect.x = 0;
+        stretchRect.y = 0;
+        stretchRect.w = SCREEN_WIDTH;
+        stretchRect.h = SCREEN_HEIGHT;
+        SDL_BlitScaled(stretchedImage, NULL, surface, &stretchRect);
+        SDL_UpdateWindowSurface(window);
     }
     close();
     return 0;
