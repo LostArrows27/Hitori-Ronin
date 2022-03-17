ba2ec93346bb135752570211a1859d43d2988c03
Nguyễn Thành Dũng <thelastofus2isnuts@gmail.com>
2022-03-17 16:50:39 +0700

    Print out PNG image file
    print out image using IMG_Load and SDL_Image.h

--- Cpp Game with SDL/Remember and Click.cpp
+++ Cpp Game with SDL/Remember and Click.cpp
@@ -1,41 +1,30 @@
-#include<iostream>
 #include<SDL.h>
+#include<SDL_image.h>
+#include<iostream>
+#include<string>
 
-const int SCREEN_HEIGHT = 800;
-const int SCREEN_WIDTH = 800;
+using namespace std;
+
+const int SCREEN_WIDTH = 960;
+const int SCREEN_HEIGHT = 595;
 
 SDL_Window* window = NULL;
 SDL_Surface* surface = NULL;
-
 SDL_Surface* stretchedImage = NULL;
 
-void init()
-{
-    window = SDL_CreateWindow("Convert Image", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    surface = SDL_GetWindowSurface(window);
-}
+void init();
+// tao cua so va surface
 
-SDL_Surface* loadSurface(std::string path)
-{
-    SDL_Surface* optimized = NULL;
-    SDL_Surface* temp = SDL_LoadBMP(path.c_str());
-    optimized = SDL_ConvertSurface(temp, surface->format, 0);
-    SDL_FreeSurface(temp);
-    return optimized;
-}
+SDL_Surface* loadSurface(string path);
+// dua anh len surface
 
-void close()
-{
-    SDL_FreeSurface(stretchedImage);
-    stretchedImage = NULL;
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-}
+void close();
+// giai phong chuong trinh
 
 int main(int argc, char* args[])
 {
     init();
-    stretchedImage = loadSurface("5th_lesson_image/stretch.bmp");
+    stretchedImage = loadSurface("Rem.png");
     bool quit = false;
     SDL_Event e;
     while(!quit)
@@ -44,14 +33,37 @@ int main(int argc, char* args[])
         {
             if(e.type == SDL_QUIT) quit = true;
         }
-        SDL_Rect stretchRect;
-        stretchRect.x = 0;
-        stretchRect.y = 0;
-        stretchRect.w = SCREEN_WIDTH;
-        stretchRect.h = SCREEN_HEIGHT;
-        SDL_BlitScaled(stretchedImage, NULL, surface, &stretchRect);
+        SDL_Rect temp;
+        temp.x = 0;
+        temp.y = 0;
+        temp.w = SCREEN_WIDTH;
+        temp.h = SCREEN_HEIGHT;
+        SDL_BlitScaled(stretchedImage, NULL, surface, &temp);
         SDL_UpdateWindowSurface(window);
     }
     close();
     return 0;
+}
+
+void init()
+{
+    window = SDL_CreateWindow("Waifu", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    surface = SDL_GetWindowSurface(window);
+}
+
+SDL_Surface* loadSurface(string path)
+{
+    SDL_Surface* stretched = NULL;
+    SDL_Surface* temp = IMG_Load(path.c_str());
+    stretched = SDL_ConvertSurface(temp, surface->format, 0);
+    SDL_FreeSurface(temp);
+    return stretched;
+}
+
+void close()
+{
+    SDL_FreeSurface(stretchedImage);
+    stretchedImage = NULL;
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
