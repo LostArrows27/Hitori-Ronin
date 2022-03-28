34905fee5453f7d1ca42e2c2f1655f0b7455b93c
Nguyễn Thành Dũng <thelastofus2isnuts@gmail.com>
2022-03-28 23:10:37 +0700

    Image appear in different place
    Using SDL_RenderSetViewport

--- Cpp Game with SDL/Header/Game_Size.h
+++ Cpp Game with SDL/Header/Game_Size.h
@@ -3,6 +3,6 @@
 using namespace std;
 
 
-const int SCREEN_WIDTH = 960;
-const int SCREEN_HEIGHT = 595;
+const int SCREEN_WIDTH = 1920/2;
+const int SCREEN_HEIGHT = 1080/2;
 const char* WINDOW_TITLE = "Printing Image";
--- Cpp Game with SDL/Remember and Click.cpp
+++ Cpp Game with SDL/Remember and Click.cpp
@@ -2,14 +2,17 @@
 #include<SDL_image.h>
 #include<string>
 #include<iostream>
-#include "Header/Close.h"
-#include "Header/Game_Size.h"
+#include"Header/Close.h"
+#include"Header/Game_Size.h"
 
 using namespace std;
 
 SDL_Window* window = NULL;
 SDL_Renderer* render = NULL;
-SDL_Texture* texture = NULL;
+SDL_Texture* texture_1 = NULL;
+SDL_Texture* texture_2 = NULL;
+SDL_Texture* texture_3 = NULL;
+SDL_Texture* texture_4 = NULL;
 
 void init();
 
@@ -18,7 +21,10 @@ SDL_Texture* loadSurface(string path);
 int main(int argc, char* args[])
 {
     init();
-    texture = loadSurface("Image/hanabi.jpg");
+    texture_1 = loadSurface("Image/001.jpg");
+    texture_2 = loadSurface("Image/002.jpg");
+    texture_3 = loadSurface("Image/003.jpg");
+    texture_4 = loadSurface("Image/004.jpg");
     bool quit = false;
     SDL_Event e;
     while(!quit)
@@ -27,45 +33,60 @@ int main(int argc, char* args[])
         {
             if(e.type == SDL_QUIT) quit = true;
         }
-                //Clear screen
-				SDL_SetRenderDrawColor( render, 0xFF, 0xFF, 0xFF, 0xFF );
-				SDL_RenderClear( render );
-				//Render red filled quad
-				SDL_Rect fillRect = { SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
-				SDL_SetRenderDrawColor( render, 0xFF, 0x00, 0x00, 0xFF );
-				SDL_RenderFillRect( render, &fillRect );
-				//Render green outlined quad
-				SDL_Rect outlineRect = { SCREEN_WIDTH / 6, SCREEN_HEIGHT / 6, SCREEN_WIDTH * 2 / 3, SCREEN_HEIGHT * 2 / 3 };
-				SDL_SetRenderDrawColor( render, 0x00, 0xFF, 0x00, 0xFF );
-				SDL_RenderDrawRect( render, &outlineRect );
-				//Draw blue horizontal line
-				SDL_SetRenderDrawColor( render, 0x00, 0x00, 0xFF, 0xFF );
-				SDL_RenderDrawLine( render, 0, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2 );
-				//Draw vertical line of yellow dots
-				SDL_SetRenderDrawColor( render, 0xFF, 0xFF, 0x00, 0xFF );
-				for( int i = 0; i < SCREEN_HEIGHT; i += 4 )
-				{
-					SDL_RenderDrawPoint( render, SCREEN_WIDTH / 2, i );
-				}
-				//Update screen
-				SDL_RenderPresent( render );
+        SDL_SetRenderDrawColor(render, 0xFF, 0xFF, 0xFF, 0xFF);
+        SDL_RenderClear(render);
+        SDL_Rect temp;
+        temp.x = 0;
+        temp.y = 0;
+        temp.h = SCREEN_HEIGHT/2;
+        temp.w = SCREEN_WIDTH/2;
+        SDL_RenderSetViewport(render, &temp);
+        SDL_RenderCopy(render, texture_1, NULL, NULL);
+        ////
+        SDL_Rect temp2;
+        temp2.x = SCREEN_WIDTH/2;
+        temp2.y = 0;
+        temp2.h = SCREEN_HEIGHT/2;
+        temp2.w = SCREEN_WIDTH/2;
+        SDL_RenderSetViewport(render, &temp2);
+        SDL_RenderCopy(render, texture_2, NULL, NULL);
+        ////
+        SDL_Rect temp3;
+        temp3.x = 0;
+        temp3.y = SCREEN_HEIGHT/2;
+        temp3.h = SCREEN_HEIGHT/2;
+        temp3.w = SCREEN_WIDTH/2;
+        SDL_RenderSetViewport(render, &temp3);
+        SDL_RenderCopy(render, texture_3, NULL, NULL);
+        ///
+        SDL_Rect temp4;
+        temp4.x = SCREEN_WIDTH/2;
+        temp4.y = SCREEN_HEIGHT/2;
+        temp4.h = SCREEN_HEIGHT/2;
+        temp4.w = SCREEN_WIDTH/2;
+        SDL_RenderSetViewport(render, &temp4);
+        SDL_RenderCopy(render, texture_4, NULL, NULL);
+        SDL_RenderPresent(render);
     }
+    close(window, render, texture_1);
+    close(window, render, texture_2);
+    close(window, render, texture_3);
+    close(window, render, texture_4);
 }
 
 void init()
 {
     window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     render = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    SDL_SetRenderDrawColor(render, 0xFF, 0xFF, 0xFF, 0xFF);
+    SDL_SetRenderDrawColor(render,0xFF, 0xFF, 0xFF, 0xFF);
 }
 
 SDL_Texture* loadSurface(string path)
 {
     SDL_Texture* temp = NULL;
-    SDL_Surface* surface_temp = IMG_Load(path.c_str());
-    temp = SDL_CreateTextureFromSurface(render, surface_temp);
-    SDL_FreeSurface(surface_temp);
+    SDL_Surface* temp_surface = IMG_Load(path.c_str());
+    temp = SDL_CreateTextureFromSurface(render, temp_surface);
+    SDL_FreeSurface(temp_surface);
     return temp;
 }
-
 
