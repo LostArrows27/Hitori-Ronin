7aa4ba2530468fb08b4a39e46f4a8d2a040412f9
Nguyễn Thành Dũng <thelastofus2isnuts@gmail.com>
2022-04-04 22:33:29 +0700

    Loading Music

--- Cpp Game with SDL/Remember and Click.cpp
+++ Cpp Game with SDL/Remember and Click.cpp
@@ -1,29 +1,28 @@
 #include<SDL.h>
 #include<SDL_image.h>
+#include<SDL_mixer.h>
 #include<iostream>
 #include<string>
 #include "Header/Game_Size.h"
 #include "Header/Texture.h"
-#include "Header/Mouse.h"
 
 using namespace std;
 
 SDL_Window* window = NULL;
 SDL_Renderer* render = NULL;
 
-SDL_Rect gSpriteClips[ BUTTON_SPRITE_TOTAL ]; // phan texture muon lay
+Texture background;
 
-Texture gSpriteSheetTexture;
+Mix_Music *gMusic = NULL;
+Mix_Chunk *gScratch = NULL;
+Mix_Chunk *gHigh = NULL;
+Mix_Chunk *gMedium = NULL;
+Mix_Chunk *gLow = NULL;
 
-SDL_RendererFlip flipType = SDL_FLIP_NONE;
-
-Button gButtons[ TOTAL_BUTTONS ];
-
-int k = SCREEN_WIDTH/2;
 
 void init();
 
-void loadMedia();
+void loadMedia(); // containing music and image
 
 void closing();
 
@@ -37,23 +36,41 @@ int main(int argc, char* args[])
     {
         while(SDL_PollEvent(&e) != 0)
         {
-            if(e.type == SDL_QUIT)
+            if(e.type == SDL_QUIT) quit = true;
+            else if(e.type == SDL_KEYDOWN)
             {
-                quit = true;
+               switch( e.key.keysym.sym )
+						{
+							case SDLK_1:
+							Mix_PlayChannel( -1, gHigh, 0 );
+							break;
+							case SDLK_2:
+							Mix_PlayChannel( -1, gMedium, 0 );
+							break;
+							case SDLK_3:
+							Mix_PlayChannel( -1, gLow, 0 );
+							break;
+							case SDLK_4:
+							Mix_PlayChannel( -1, gScratch, 0 );
+							break;
+							case SDLK_9:
+							if( Mix_PlayingMusic() == 0 ) Mix_PlayMusic( gMusic, -1 );
+							else
+							{
+								if( Mix_PausedMusic() == 1 ) Mix_ResumeMusic();
+								else Mix_PauseMusic();
+							}
+							break;
+							case SDLK_0:
+							Mix_HaltMusic();
+							break;
+						}
             }
-            for(int i = 0; i < TOTAL_BUTTONS; ++i)
-            {
-                gButtons[i].handle_mouseEvent(&e);
-            }
+            SDL_SetRenderDrawColor(render, 255, 255 ,255 ,255);
+            SDL_RenderClear(render);
+            background.renderer(0, 0, render, NULL);
+            SDL_RenderPresent(render);
         }
-        SDL_SetRenderDrawColor(render, 255, 255 ,255 ,255);
-        SDL_RenderClear(render);
-        for(int i = 0; i < TOTAL_BUTTONS; ++i)
-        {
-            int mouse_state = gButtons[i].get_currentSprite();
-            gButtons[i].render_button(gSpriteSheetTexture, render, gSpriteClips[mouse_state]);
-        }
-        SDL_RenderPresent(render);
     }
     closing();
     return 0;
@@ -64,26 +81,32 @@ void init()
     window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     render = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     SDL_SetRenderDrawColor(render, 0xFF, 0xFF, 0xFF, 0xFF);
+    Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
 }
 
 void loadMedia()
 {
-        gSpriteSheetTexture.loadImage("Image/button.png", render);
-        for(int i = 0; i < BUTTON_SPRITE_TOTAL; ++i){
-            gSpriteClips[ i ].x =  0;
-            gSpriteClips[ i ].y =  i * 200;
-            gSpriteClips[ i ].w = BUTTON_WIDTH;
-            gSpriteClips[ i ].h = BUTTON_HEIGHT;
-        }
-        gButtons[ 0 ].setPosition(0, 0);
-		gButtons[ 1 ].setPosition( SCREEN_WIDTH - BUTTON_WIDTH, 0 );
-		gButtons[ 2 ].setPosition( 0, SCREEN_HEIGHT - BUTTON_HEIGHT );
-		gButtons[ 3 ].setPosition( SCREEN_WIDTH - BUTTON_WIDTH, SCREEN_HEIGHT - BUTTON_HEIGHT );
+        background.loadImage("Image/prompt.png", render);
+        gMusic = Mix_LoadMUS( "Sound/beat.wav" );
+        gScratch = Mix_LoadWAV( "Sound/scratch.wav" );
+        gHigh = Mix_LoadWAV( "Sound/high.wav" );
+        gMedium = Mix_LoadWAV( "Sound/medium.wav" );
+        gLow = Mix_LoadWAV( "Sound/low.wav" );
 }
 
 void closing()
 {
-    gSpriteSheetTexture.free();
+    background.free();
+    Mix_FreeChunk( gScratch );
+	Mix_FreeChunk( gHigh );
+	Mix_FreeChunk( gMedium );
+	Mix_FreeChunk( gLow );
+	gScratch = NULL;
+	gHigh = NULL;
+	gMedium = NULL;
+	gLow = NULL;
+	Mix_FreeMusic( gMusic );
+	gMusic = NULL;
     SDL_DestroyRenderer(render);
     SDL_DestroyWindow(window);
     window = NULL;
@@ -91,5 +114,4 @@ void closing()
     IMG_Quit();
     SDL_Quit();
 }
-
 
