98e75635aacfe6138211008fa1575a769f06bf80
Nguyễn Thành Dũng <thelastofus2isnuts@gmail.com>
2022-04-07 10:18:11 +0700

    Time to 0.00 format

--- Cpp Game with SDL/Remember and Click.cpp
+++ Cpp Game with SDL/Remember and Click.cpp
@@ -13,7 +13,7 @@ int main(int argc, char* args[])
     Texture time_text;
     Texture bg;
     Common_Func::init();
-    bg.loadMedia("Font/Need for font.ttf", "YOUR GAME HAS BEGIN", 28, 255, 0, 0);
+    bg.loadMedia("Font/Need for font.ttf", "your game has begin", 30, 255, 0, 0);
     bool quit = false;
     SDL_Event e;
     Uint32 startTime = 0;
@@ -25,12 +25,14 @@ int main(int argc, char* args[])
             if(e.type == SDL_QUIT) quit = true;
             else if(e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_RETURN) startTime = SDL_GetTicks();
         }
-        int k = (SDL_GetTicks() - startTime)/100;
+
+        int k = (SDL_GetTicks() - startTime)/10;
         int dv = k%10;
-        int chuc = (k-dv)/10;
+        int next = ((k-dv)/10) % 10;
+        int chuc = (k-dv-next*10)/100;
         timeText.str( "" );
-        timeText << "TIME: " << chuc << "." << dv;
-        time_text.loadMedia("Font/Need for font.ttf", timeText.str().c_str(), 40, 255, 0 , 255);
+        timeText << "TIME: " << chuc << "." << next << dv;
+        time_text.loadMedia("Font/Mistral.ttf", timeText.str().c_str(), 40, 255, 0 , 255);
         Common_Func::back_render();
         time_text.onscreen(0, 0);
         bg.onscreen(100,200);
