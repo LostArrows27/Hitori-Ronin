920ee87e4218692a5d214d7a300dec262af9fc59
Nguyễn Thành Dũng <thelastofus2isnuts@gmail.com>
2022-04-19 20:46:17 +0700

    Add Vector and Texture Manager Library

---
+++ Cpp Game with SDL/Header/Vector 2D.h
@@ -0,0 +1,16 @@
+#ifndef VECTOR 2D_H
+#define VECTOR 2D_H
+
+
+class Vector 2D
+{
+    public:
+        Vector 2D() {}
+        virtual ~Vector 2D() {}
+
+    protected:
+
+    private:
+};
+
+#endif // VECTOR 2D_H
