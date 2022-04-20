b8c875b0fbee05990e4861c7df6b9a6d0d97c8ea
Nguyễn Thành Dũng <thelastofus2isnuts@gmail.com>
2022-04-20 23:28:37 +0700

    Fix the "Quay dau" bug
    bug where the character wont turn his direction to the same direction as he turn

--- Cpp Game with SDL/Header/Warrior.cpp
+++ Cpp Game with SDL/Header/Warrior.cpp
@@ -3,6 +3,9 @@
 #include "SDL.h"
 #include "Input.h"
 
+static SDL_RendererFlip state = SDL_FLIP_NONE;
+// u can comment this "state" value if error to the youtube channel and remove all the "state" below :D
+
 Warrior::Warrior(Properties* props) : Character(props)
 {
     //m_Row = 1;
@@ -10,7 +13,7 @@ Warrior::Warrior(Properties* props) : Character(props)
     //m_AnimSpeed = 80;
     m_RigidBody = new RigidBody();
     m_Animation = new Animation();
-    m_Animation->SetProps(m_TextureID, 1, 8, 100); // 80 la toc do cua chuyen dong
+    m_Animation->SetProps(m_TextureID, 1, 8, 80); // 80 la toc do cua chuyen dong
 }
 
 void Warrior::Draw()
@@ -20,18 +23,20 @@ void Warrior::Draw()
 
 void Warrior::Update(float dt)
 {
-    m_Animation->SetProps("player", 1 ,8 ,100);
+    m_Animation->SetProps("player", 1 ,8 ,100, state);
     m_RigidBody->UnSetForce();
 
     if(Input::GetInstance()->GetKeyDown(SDL_SCANCODE_A))
     {
         m_RigidBody->ApplyForceX(5*BACKWARD);
-        m_Animation->SetProps("player_run", 1, 8, 100, SDL_FLIP_HORIZONTAL);
+        state = SDL_FLIP_HORIZONTAL;
+        m_Animation->SetProps("player_run", 1, 8, 100, state);
     }
     if(Input::GetInstance()->GetKeyDown(SDL_SCANCODE_D))
     {
         m_RigidBody->ApplyForceX(5*FORWARD); // chinh toc do backward va forward de chinh toc do di chuyen
-        m_Animation->SetProps("player_run", 1, 8, 100);
+        state = SDL_FLIP_NONE;
+        m_Animation->SetProps("player_run", 1, 8, 100, state);
     }
     SDL_Log("%f", dt);
     m_RigidBody->Update(dt);
